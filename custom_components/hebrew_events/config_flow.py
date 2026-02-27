b6ccb450a709c2f79e7d1d66c5e53aa41591c136
abfist <abfist@yahoo.com>
2026-02-27 13:16:57 +0200

    updated config folow

--- custom_components/hebrew_events/config_flow.py
+++ custom_components/hebrew_events/config_flow.py
@@ -1,9 +1,10 @@
-import voluptuous as vol
 from homeassistant import config_entries
+import voluptuous as vol
+
 from .const import DOMAIN
 
-class HebrewEventsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
+class HebrewEventsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
@@ -14,11 +15,16 @@ class HebrewEventsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             )
 
         schema = vol.Schema({
-            vol.Required("type"): vol.In(["yahrzeit", "birthday","holy-day","other"]),
             vol.Required("name"): str,
-            vol.Required("day"): int,
-            vol.Required("month"): int,
-            vol.Optional("reminders", default=""): str
+            vol.Required("type"): vol.In(["yahrzeit", "birthday"]),
+            vol.Required("day"): vol.All(int, vol.Range(min=1, max=30)),
+            vol.Required("month"): vol.All(int, vol.Range(min=1, max=13)),
+            vol.Optional("adar_type", default="auto"):
+                vol.In(["auto", "adar1", "adar2"]),
+            vol.Optional("reminders", default=""): str,
         })
 
-        return self.async_show_form(step_id="user", data_schema=schema)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema
+        )
